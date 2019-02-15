ac890c4b2782ce91332e7cdb0057cb157aea9206
Yuki Shimada <emadurandal@gmail.com>
2019-02-15 14:44:41 +0900

    Complete chapter 2

--- MyRaytracing/MyRaytracing/main.cpp
+++ MyRaytracing/MyRaytracing/main.cpp
@@ -7,6 +7,7 @@
 //
 
 #include <iostream>
+#include "vec3.h"
 
 int main(int argc, const char * argv[]) {
     int nx = 200;
@@ -14,12 +15,10 @@ int main(int argc, const char * argv[]) {
     std::cout << "P3\n" << nx << " " << ny << "\n255\n";
     for (int j = ny - 1; j >= 0; j--) {
         for (int i = 0; i < nx; i++) {
-            float r = float(i) / float(nx);
-            float g = float(j) / float(ny);
-            float b = 0.2;
-            int ir = int(255.99 * r);
-            int ig = int(255.99 * g);
-            int ib = int(255.99 * b);
+            vec3 col(float(i) / float(nx), float(j) / float(ny), 0.2);
+            int ir = int(255.99 * col[0]);
+            int ig = int(255.99 * col[1]);
+            int ib = int(255.99 * col[2]);
             std::cout << ir << " " << ig << " " << ib << "\n";
         }
     }
