3c384ff4c2a4251bea60a4f2de54996655479a5c
Yuki Shimada <emadurandal@gmail.com>
2019-02-16 03:06:48 +0900

    Implementing Chapter 9 (~p51)

--- MyRaytracing/MyRaytracing/main.cpp
+++ MyRaytracing/MyRaytracing/main.cpp
@@ -45,10 +45,11 @@ int main(int argc, const char * argv[]) {
     vec3 origin(0.0, 0.0, 0.0);
     
     hitable *list[4];
-    list[0] = new sphere(vec3(0, 0, -1), 0.5, new lambertian(vec3(0.8, 0.3, 0.3)));
+    list[0] = new sphere(vec3(0, 0, -1), 0.5, new lambertian(vec3(0.1, 0.2, 0.5)));
     list[1] = new sphere(vec3(0, -100.5, -1), 100, new lambertian(vec3(0.8, 0.8, 0.0)));
     list[2] = new sphere(vec3(1, 0, -1), 0.5, new metal(vec3(0.8, 0.6, 0.2), 1.0));
-    list[3] = new sphere(vec3(-1, 0, -1), 0.5, new metal(vec3(0.8, 0.8, 0.8), 0.3));
+    list[3] = new sphere(vec3(-1, 0, -1), 0.5, new dielectric(1.5));
+    
     hitable *world = new hitable_list(list,4);
     
 
--- MyRaytracing/MyRaytracing/material.h
+++ MyRaytracing/MyRaytracing/material.h
@@ -42,7 +42,6 @@ vec3 reflect(const vec3& v, const vec3& n) {
     return v - 2 * dot(v, n) * n;
 }
 
-
 class metal : public material {
 public:
     metal(const vec3& a, float f) : albedo(a) {
@@ -58,6 +57,46 @@ public:
     
     vec3 albedo;
     float fuzz;
+};
+
+
+bool refract(const vec3& v, const vec3& n, float ni_over_nt, vec3& refracted) {
+    vec3 uv = unit_vector(v);
+    float dt = dot(uv, n);
+    float discriminant = 1.0 - ni_over_nt * ni_over_nt * (1 - dt * dt);
+    if (discriminant > 0) {
+        refracted = ni_over_nt * (uv - n * dt) - n * sqrt(discriminant);
+        return true;
+    } else
+        return false;
+}
+
+class dielectric : public material {
+public:
+    dielectric(float ri) : ref_idx(ri) {}
+    virtual bool scatter(const ray& r_in, const hit_record& rec, vec3& attenuation, ray& scattered) const {
+        vec3 outward_normal;
+        vec3 reflected = reflect(r_in.direction(), rec.normal);
+        float ni_over_nt;
+        attenuation = vec3(1.0, 1.0, 0.0);
+        vec3 refracted;
+        if (dot(r_in.direction(), rec.normal) > 0) {
+            outward_normal = -rec.normal;
+            ni_over_nt = ref_idx;
+        } else {
+            outward_normal = rec.normal;
+            ni_over_nt = 1.0 / ref_idx;
+        } if (refract(r_in.direction(), outward_normal, ni_over_nt, refracted)) {
+            scattered = ray(rec.p, refracted);
+        } else {
+            scattered = ray(rec.p, reflected);
+            return false;
+        }
+        
+        return true;
+    }
+
+    float ref_idx;
 };
 
 
