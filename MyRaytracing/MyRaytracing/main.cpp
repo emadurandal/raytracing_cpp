f0e8b2ba8960a1135f34a6af3541e27d24a4672d
Yuki Shimada <emadurandal@gmail.com>
2019-02-15 20:43:32 +0900

    Complete chapter 7

--- MyRaytracing/MyRaytracing/main.cpp
+++ MyRaytracing/MyRaytracing/main.cpp
@@ -14,17 +14,29 @@
 #include "camera.h"
 
 
+vec3 random_in_unit_sphere() {
+    vec3 p;
+    
+    do {
+        p = 2.0 * vec3(drand48(), drand48(), drand48()) - vec3(1, 1, 1);
+    } while (p.squared_length() >= 1.0);
+    
+    return p;
+    
+}
+
 vec3 color(const ray& r, hitable *world) {
     hit_record rec;
-    if (world->hit(r, 0.0, MAXFLOAT, rec)) {
-        return 0.5 * vec3(rec.normal.x() + 1, rec.normal.y() + 1, rec.normal.z() + 1);
+    
+    if (world->hit(r, 0.001, MAXFLOAT, rec)) {
+        vec3 target = rec.p + rec.normal + random_in_unit_sphere();
+        return 0.5 * color(ray(rec.p, target-rec.p), world);
     } else {
         vec3 unit_direction = unit_vector(r.direction());
         float t = 0.5 * (unit_direction.y() + 1.0);
         return (1.0 - t) * vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0);
     }
 }
-
 
 int main(int argc, const char * argv[]) {
     int nx = 200;
@@ -55,6 +67,7 @@ int main(int argc, const char * argv[]) {
                 col += color(r, world);
             }
             col /= float(ns);
+            col = vec3(sqrt(col[0]), sqrt(col[1]), sqrt(col[2]));
             
             int ir = int(255.99 * col[0]);
             int ig = int(255.99 * col[1]);
