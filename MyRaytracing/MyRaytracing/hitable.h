c26feb347c7138eeb93f80f1dc0a2e06ff18a3d8
Yuki Shimada <emadurandal@gmail.com>
2019-02-15 19:49:40 +0900

    Complete chapter 5

---
+++ MyRaytracing/MyRaytracing/hitable.h
@@ -0,0 +1,25 @@
+//
+//  hitable.h
+//  MyRaytracing
+//
+//  Created by emadurandal on 2019/02/15.
+//  Copyright © 2019 Yuki Shimada. All rights reserved.
+//
+
+#ifndef hitable_h
+#define hitable_h
+
+#include "ray.h"
+
+struct hit_record {
+    float t;
+    vec3 p;
+    vec3 normal;
+};
+
+class hitable {
+public:
+    virtual bool hit(const ray& r, float t_min, float t_max, hit_record& rec) const = 0;
+};
+
+#endif /* hitable_h */
---
+++ MyRaytracing/MyRaytracing/sphere.h
@@ -0,0 +1,50 @@
+//
+//  sphere.h
+//  MyRaytracing
+//
+//  Created by emadurandal on 2019/02/15.
+//  Copyright © 2019 Yuki Shimada. All rights reserved.
+//
+
+#ifndef sphere_h
+#define sphere_h
+
+#include "hitable.h"
+class sphere: public hitable {
+    public: sphere() {}
+    sphere(vec3 cen, float r) : center(cen), radius(r) {};
+    virtual bool hit(const ray& r, float tmin, float tmax, hit_record& rec) const;
+    vec3 center;
+    float radius;
+    
+};
+
+bool sphere::hit(const ray& r, float t_min, float t_max, hit_record& rec) const {
+    vec3 oc = r.origin() - center;
+    float a = dot(r.direction(), r.direction());
+    float b = dot(oc, r.direction());
+    float c = dot(oc, oc) - radius * radius;
+    float discriminant = b * b - a * c;
+    
+    if (discriminant > 0) {
+        float temp = (-b - sqrt(discriminant)) / a;
+        if (temp < t_max && temp > t_min) {
+            rec.t = temp; rec.p = r.point_at_parameter(rec.t);
+            rec.normal = (rec.p - center) / radius;
+            
+            return true;
+            
+        }
+        temp = (-b + sqrt(discriminant)) / a;
+        if (temp < t_max && temp > t_min) {
+            rec.t = temp; rec.p = r.point_at_parameter(rec.t);
+            rec.normal = (rec.p - center) / radius; return true;
+            
+        }
+        
+    }
+    return false;
+    
+}
+
+#endif /* sphere_h */
