de9b650484b3db86d663acccf8cb5f62f954f305
Yuki Shimada <emadurandal@gmail.com>
2019-02-15 14:27:39 +0900

    Add XCode Project

---
+++ MyRaytracing/MyRaytracing/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MyRaytracing
+//
+//  Created by emadurandal on 2019/02/15.
+//  Copyright © 2019 Yuki Shimada. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
