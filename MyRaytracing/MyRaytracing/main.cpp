43082b994663d8eed7a861cee5ef2bc1bf2a14d2
Yuki Shimada <emadurandal@gmail.com>
2019-02-16 03:53:16 +0900

    Complete chapter 12

--- MyRaytracing/MyRaytracing/main.cpp
+++ MyRaytracing/MyRaytracing/main.cpp
@@ -31,6 +31,33 @@ vec3 color(const ray& r, hitable *world, int depth) {
     }
 }
 
+hitable *random_scene() {
+    int n = 500;
+    hitable **list = new hitable*[n+1];
+    list[0] = new sphere(vec3(0, -1000, 0), 1000, new lambertian( vec3(0.5, 0.5, 0.5)));
+    int i = 1;
+    for (int a = -11; a < 11; a++) {
+        for (int b = -11; b < 11; b++) {
+            float choose_mat = drand48();
+            vec3 center(a + 0.9 * drand48(), 0.2, b + 0.9 * drand48());
+            if ((center - vec3(4, 0.2, 0)).length() > 0.9) {
+                if (choose_mat < 0.8) {// diffuse
+                    list[i++] = new sphere(center, 0.2, new lambertian( vec3(drand48() * drand48(), drand48() * drand48(), drand48() * drand48())));
+                } else if (choose_mat < 0.95) { // metal
+                    list[i++] = new sphere( center, 0.2, new metal( vec3( 0.5 * (1 + drand48()), 0.5 * (1 + drand48()), 0.5 * (1 + drand48()) ), 0.5 * drand48()));
+                } else { // glass
+                    list[i++] = new sphere(center, 0.2, new dielectric(1.5));
+                }
+            }
+        }
+    }
+    list[i++] = new sphere(vec3(0, 1, 0), 1.0, new dielectric(1.5));
+    list[i++] = new sphere(vec3(-4, 1, 0), 1.0, new lambertian(vec3(0.4, 0.2, 0.1)));
+    list[i++] = new sphere(vec3(4, 1, 0), 1.0, new metal(vec3(0.7, 0.6, 0.5), 0.0));
+    
+    return new hitable_list(list,i);
+}
+        
 
 int main(int argc, const char * argv[]) {
     int nx = 200;
@@ -39,16 +66,14 @@ int main(int argc, const char * argv[]) {
     std::ofstream outputfile("test.ppm");
     outputfile << "P3\n" << nx << " " << ny << "\n255\n";
     
-    vec3 lower_left_corner(-2.0, -1.0, -1.0);
-    vec3 horizontal(4.0, 0.0, 0.0);
-    vec3 vertical(0.0, 2.0, 0.0);
-    vec3 origin(0.0, 0.0, 0.0);
-    
-    hitable *list[2];
-    float R = cos(M_PI / 4);
-    list[0] = new sphere(vec3(-R, 0, -1), R, new lambertian(vec3(0, 0, 1)));
-    list[1] = new sphere(vec3( R, 0, -1), R, new lambertian(vec3(1, 0, 0)));
-    hitable *world = new hitable_list(list, 2);
+    hitable *list[5];
+    list[0] = new sphere(vec3(0,0,-1), 0.5, new lambertian(vec3(0.1, 0.2, 0.5)));
+    list[1] = new sphere(vec3(0,-100.5,-1), 100, new lambertian(vec3(0.8, 0.8, 0.0)));
+    list[2] = new sphere(vec3(1,0,-1), 0.5, new metal(vec3(0.8, 0.6, 0.2), 0.0));
+    list[3] = new sphere(vec3(-1,0,-1), 0.5, new dielectric(1.5));
+    list[4] = new sphere(vec3(-1,0,-1), -0.45, new dielectric(1.5));
+    hitable *world = new hitable_list(list,5);
+    world = random_scene();
         
     vec3 lookfrom(3, 3, 2); vec3 lookat(0, 0, -1);
     float dist_to_focus = (lookfrom - lookat).length();
