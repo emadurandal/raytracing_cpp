e36a6552666b1d485b6e690da024926c884ad802
Yuki Shimada <emadurandal@gmail.com>
2019-02-16 03:16:07 +0900

    Complete chapter 9

--- MyRaytracing/MyRaytracing/main.cpp
+++ MyRaytracing/MyRaytracing/main.cpp
@@ -44,13 +44,14 @@ int main(int argc, const char * argv[]) {
     vec3 vertical(0.0, 2.0, 0.0);
     vec3 origin(0.0, 0.0, 0.0);
     
-    hitable *list[4];
+    hitable *list[5];
     list[0] = new sphere(vec3(0, 0, -1), 0.5, new lambertian(vec3(0.1, 0.2, 0.5)));
     list[1] = new sphere(vec3(0, -100.5, -1), 100, new lambertian(vec3(0.8, 0.8, 0.0)));
     list[2] = new sphere(vec3(1, 0, -1), 0.5, new metal(vec3(0.8, 0.6, 0.2), 1.0));
     list[3] = new sphere(vec3(-1, 0, -1), 0.5, new dielectric(1.5));
+    list[4] = new sphere(vec3(-1, 0, -1), -0.45, new dielectric(1.5));
     
-    hitable *world = new hitable_list(list,4);
+    hitable *world = new hitable_list(list,5);
     
 
     camera cam;
